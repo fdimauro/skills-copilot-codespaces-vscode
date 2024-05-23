55d2bee8a9ce1b12e0801568c34cfdc619ff0f5d
Frank DiMauro <frank.dimauro5@gmail.com>
2024-05-23 18:41:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,45 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: "HTML",
+            icon: "fab fa-html5",
+            level: 90
+        },
+        {
+            name: "CSS",
+            icon: "fab fa-css3-alt",
+            level: 90
+        },
+        {
+            name: "JavaScript",
+            icon: "fab fa-js-square",
+            level: 70
+        },
+        {
+            name: "React",
+            icon: "fab fa-react",
+            level: 70
+        },
+        {
+            name: "Node.js",
+            icon: "fab fa-node-js",
+            level: 60
+        },
+        {
+            name: "MongoDB",
+            icon: "fas fa-database",
+            level: 60
+        },
+        {
+            name: "Express.js",
+            icon: "fab fa-node-js",
+            level: 60
+        },
+        {
+            name: "Bootstrap",
+            icon: "fab fa-bootstrap",
+            level: 90
+        }
+    ];
+    return skills;
+}
